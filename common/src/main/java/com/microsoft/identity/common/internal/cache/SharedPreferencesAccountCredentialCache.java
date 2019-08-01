2e8a3a8b643b76485b28cb781daa675bca64ed03
Markus Schweizer <markus.schweizer@ffuf.de>
2019-08-01 11:48:20 +0200

    nullpointer hotfix

--- common/src/main/java/com/microsoft/identity/common/internal/cache/SharedPreferencesAccountCredentialCache.java
+++ common/src/main/java/com/microsoft/identity/common/internal/cache/SharedPreferencesAccountCredentialCache.java
@@ -196,7 +196,7 @@ public class SharedPreferencesAccountCredentialCache extends AbstractAccountCred
 
         for (Map.Entry<String, ?> cacheValue : cacheValues.entrySet()) {
             final String cacheKey = cacheValue.getKey();
-            if (isAccount(cacheKey)) {
+            if (isAccount(cacheKey) && cacheValue.getValue() != null) {
                 final AccountRecord account = mCacheValueDelegate.fromCacheValue(
                         cacheValue.getValue().toString(),
                         AccountRecord.class
@@ -210,6 +210,8 @@ public class SharedPreferencesAccountCredentialCache extends AbstractAccountCred
                 } else {
                     accounts.put(cacheKey, account);
                 }
+            } else if (cacheValue.getValue() == null) {
+                Logger.warn(TAG, "caught msal nullpointer error");
             }
         }
 
@@ -258,7 +260,7 @@ public class SharedPreferencesAccountCredentialCache extends AbstractAccountCred
 
         for (Map.Entry<String, ?> cacheValue : cacheValues.entrySet()) {
             final String cacheKey = cacheValue.getKey();
-            if (isCredential(cacheKey)) {
+            if (isCredential(cacheKey) && cacheValue.getValue() != null) {
                 final Credential credential = mCacheValueDelegate.fromCacheValue(
                         cacheValue.getValue().toString(),
                         credentialClassForType(cacheKey)
@@ -272,6 +274,8 @@ public class SharedPreferencesAccountCredentialCache extends AbstractAccountCred
                 } else {
                     credentials.put(cacheKey, credential);
                 }
+            } else if (cacheValue.getValue() == null) {
+                Logger.warn(TAG, "caught msal nullpointer error");
             }
         }
 
